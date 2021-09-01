a48cc93d8de74d6904cec23228e32fbbbcd7dee9
mangster80 <magnus.wallin80@gmail.com>
2021-09-01 23:44:23 +0200

    move footer inside body, add date

--- code/script.js
+++ code/script.js
@@ -1,9 +1,17 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
+
+const d = new Date();
+document.getElementById('year').innerHTML = d.getFullYear();
+
 function toggle() {
-  this.classList.toggle("active")
+  this.classList.toggle('active');
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById('section1').onclick = toggle;
+
+const date = new Date();
+console.log(date);
+document.getElementById('year').innerHTML = date.getFullYear();
